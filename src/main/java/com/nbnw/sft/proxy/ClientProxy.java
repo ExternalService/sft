09b3cb9befdf362948fc430f30cbdca63bcecfbd
test <3121572062@qq.com>
2023-12-01 22:03:30 +0800

    Language Manager

--- src/main/java/com/nbnw/sft/proxy/ClientProxy.java
+++ src/main/java/com/nbnw/sft/proxy/ClientProxy.java
@@ -1,9 +1,11 @@
 package com.nbnw.sft.proxy;
 
+import com.nbnw.sft.common.LangManager;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
 
@@ -23,7 +25,7 @@ public class ClientProxy extends CommonProxy {
     public void init() {
         super.init(); // 调用公共代理的初始化代码
         // 注册快捷键
-        toggleSleepKeyBinding = new KeyBinding("key.toggleSleepKey", Keyboard.KEY_P, "key.categories.sft");
+        toggleSleepKeyBinding = new KeyBinding(I18n.format(LangManager.sleepToggle), Keyboard.KEY_P, I18n.format(LangManager.keyCategories));
         ClientRegistry.registerKeyBinding(toggleSleepKeyBinding); // 注册按键绑定
         // register the key bending and event listener on client side.
         FMLCommonHandler.instance().bus().register(this);
@@ -34,7 +36,6 @@ public class ClientProxy extends CommonProxy {
         if (toggleSleepKeyBinding.isPressed()) { // 检查绑定的按键是否按下，并处理它
             // 因为按键代码会随玩家设置改变，所以不能发绑定的按键的代码，需要为不同的按键手动给数据包分配不同的keyCode
             int singleSleepKeyCode = PacketManager.getInstance().getSingleSleepKeyCode();
-            System.out.println("Several Player Sleep Key Code:" + singleSleepKeyCode);
             ModEntry.network.sendToServer(new CommonMessagePacket(MessageType.CLIENT_KEY_PRESSED_CODE, singleSleepKeyCode));
         }
     }
