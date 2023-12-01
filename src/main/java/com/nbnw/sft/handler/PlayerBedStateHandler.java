ccc32d3d616fcff1147830671200e75f34e9883d
test <3121572062@qq.com>
2023-12-01 22:04:12 +0800

    update

--- src/main/java/com/nbnw/sft/handler/PlayerBedStateHandler.java
+++ src/main/java/com/nbnw/sft/handler/PlayerBedStateHandler.java
@@ -1,9 +1,11 @@
 package com.nbnw.sft.handler;
 
+import com.nbnw.sft.common.LangManager;
 import com.nbnw.sft.common.PlayerCountUtil;
 import com.nbnw.sft.config.ModConfig;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
@@ -19,9 +21,10 @@ public class PlayerBedStateHandler {
         String worldPercentage = String.format("%.2f%%", (double) playersInBedCount / (double) playersInWorldCount * 100);
         if (player.isPlayerSleeping()) {
             String percentage = String.format("%.2f%%", ModConfig.getInstance().getSpsThreshold() * 100);
-            ScreenMessageHandler.getInstance().showMessage("",
-                    "Current World Sleeping data: " +
-                            playersInBedCount + " / " + playersInWorldCount + ". Client percentage: " + worldPercentage + " Server threshold: " + percentage,
+            ScreenMessageHandler.getInstance().showMessage(I18n.format(LangManager.sleepCountMessage) + " " +
+                            playersInBedCount + " / " + playersInWorldCount +
+                            I18n.format(LangManager.currentSleepPercentage) + worldPercentage +
+                            I18n.format(LangManager.serverThresholdPercentage) + percentage,
                     1,
                     0xE367E9);
         }
