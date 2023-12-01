8952133db5fb127a4a3379b8f4165a0003664732
test <3121572062@qq.com>
2023-12-02 00:02:26 +0800

    update

--- src/main/java/com/nbnw/sft/handler/PlayerBedStateHandler.java
+++ src/main/java/com/nbnw/sft/handler/PlayerBedStateHandler.java
@@ -23,8 +23,9 @@ public class PlayerBedStateHandler {
             String percentage = String.format("%.2f%%", ModConfig.getInstance().getSpsThreshold() * 100);
             ScreenMessageHandler.getInstance().showMessage("",I18n.format(LangManager.sleepCountMessage) + " " +
                             playersInBedCount + " / " + playersInWorldCount +
-                            I18n.format(LangManager.currentSleepPercentage) + worldPercentage +
-                            I18n.format(LangManager.serverThresholdPercentage) + percentage,
+                            I18n.format(LangManager.currentSleepPercentage) + worldPercentage ,
+                            //+ I18n.format(LangManager.serverThresholdPercentage) + percentage,
+                    // TODO 要正确显示百分比需要客户端从服务端读取配置文件数据，并且每当客户端和服务端修改配置都需要做同步才能保证显示的值是正确的，所以暂时不做这个显示
                     1,
                     0xE367E9);
         }
