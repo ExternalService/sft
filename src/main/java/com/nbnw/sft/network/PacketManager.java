046b5f44f8fca2a38c2554bc4a87e6bce34fef6f
test <3121572062@qq.com>
2025-01-17 16:43:42 +0800

    update

--- src/main/java/com/nbnw/sft/network/PacketManager.java
+++ src/main/java/com/nbnw/sft/network/PacketManager.java
@@ -2,7 +2,7 @@ package com.nbnw.sft.network;
 
 // 用于同一管理
 public class PacketManager {
-    //private static int singleSleepKeyCode = 0; // 用于客户端按下按键切换少数人睡觉功能
+    private static int singleSleepKeyCode = 0; // 用于客户端按下按键切换少数人睡觉功能
 
     private static int serverLanguageRequestKeyCode = 1; // 用于服务端向客户端请求客户端选择的本地化语言类型功能 目前没有实际的作用 可以删除这部分相关的代码
 
@@ -17,9 +17,9 @@ public class PacketManager {
         return instance;
     }
 
-//    public int getSingleSleepKeyCode(){
-//        return this.singleSleepKeyCode;
-//    }
+    public int getSingleSleepKeyCode(){
+        return this.singleSleepKeyCode;
+    }
 
     public int getServerLanguageRequestKeyCode(){
         return this.serverLanguageRequestKeyCode;
