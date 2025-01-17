2622d99e33cad9c8ff12d0699dfb106b54656b73
test <3121572062@qq.com>
2025-01-17 16:26:40 +0800

    update

--- src/main/java/com/nbnw/sft/common/PlayerCountUtil.java
+++ src/main/java/com/nbnw/sft/common/PlayerCountUtil.java
@@ -12,10 +12,10 @@ public class PlayerCountUtil {
     // 当前世界睡觉玩家人数
     public int currentWorldSleepPlayerCount(){
         int sleepPlayerCount = 0;
-        for (Object obj : world.playerEntities) { // 改为检查这个实体是否是玩家示例，这样可以避免不安全的显示类型转换
+        for (Object obj : world.playerEntities) { // 改为检查这个实体是否是玩家实例，避免不安全的显示类型转换
             if (obj instanceof EntityPlayer) {
                 EntityPlayer player = (EntityPlayer) obj;
-                if (player.isPlayerSleeping()) { // 你不能直接在PlayerSleepInBedEvent中调用该方法检测睡觉玩家数量 睡眠玩家人数会在下一个tick才更新 所以在PlayerSleepInBedEvent中使用会计数不准
+                if (player.isPlayerSleeping()) { // 不能直接在PlayerSleepInBedEvent中调用该方法检测睡觉玩家数量 睡眠玩家人数会在下一个tick才更新 所以在PlayerSleepInBedEvent中使用会计数不准
                     sleepPlayerCount++;
                 }
             }
